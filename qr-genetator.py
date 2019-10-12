a088e16f7de67c61578412d006a22349cad6d716
BossOfCreeps <seva-ra4is@mail.ru>
2019-10-12 23:27:31 +0700

    add recogniser

--- qr-genetator.py
+++ qr-genetator.py
@@ -1,6 +1,6 @@
 import pyqrcode, png, sys
 
-'''
+
 big_code = pyqrcode.create('0987654321')
 big_code.png('code.png', scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
 big_code.show()
@@ -10,3 +10,4 @@ if d.decode('code.png'):
     print ('result: ' + d.result)
 else:
     print ('error: ' + d.error)
+'''
