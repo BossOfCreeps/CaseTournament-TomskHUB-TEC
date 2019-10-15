1046941a30ea0276d898ebb87a327f5057d928da
BossOfCreeps <seva-ra4is@mail.ru>
2019-10-15 20:30:57 +0700

    everthing is awesome

--- sevaSQL.py
+++ sevaSQL.py
@@ -10,8 +10,7 @@ def select_sql():
         records = cursor.fetchall()
         connection.close()
         cursor.close()
-        '''print(len(records))
-        print("seva")
+        '''print("seva")
     
         for row in records:
             for col in row:
@@ -27,7 +26,8 @@ def insert_into_sql(data):
         cursor = connection.cursor()
         # data = (len(select_sql()) + 1,) + data
         #print("INSERT INTO `maintable` VALUES (" + str(len(select_sql()) + 1) + ", " + data + ");")
-        cursor.execute("INSERT INTO `maintable` VALUES (" + str(len(select_sql()) + 1) + ", " + data + ");")
+        records=select_sql()
+        cursor.execute("INSERT INTO `maintable` VALUES (" + str(records[len(records)-1][0] + 1) + ", " + data + ");")
         connection.commit()
         connection.close()
         cursor.close()
@@ -59,8 +59,41 @@ def delete_all_sql():
     except Error as e:
         return "Error reading data from MySQL table"+str(e)
 
+def find_sql(name):
+    try:
+        connection = mysql.connector.connect(host='34.90.47.156', database='testdb', user='seva', password='rachis')
+        sql_select_Query = "select * from maintable where name='"+name+"';"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_Query)
+        records = cursor.fetchall()
+        connection.close()
+        cursor.close()
+        '''print("seva")
 
-#data = "'polya_name', '1997-10-04', 3.14, 0.1, 0.2, 0.4, 'oxygen2', 1"
+        for row in records:
+            for col in row:
+                print(col)
+        '''
+        return records
+    except Error as e:
+        return "Error reading data from MySQL table" + str(e)
+
+def update_sql(name, date, weight, dimensions_x, dimensions_y, dimensions_z, composition, qual_control):
+    try:
+        connection = mysql.connector.connect(host='34.90.47.156', database='testdb', user='seva', password='rachis')
+        cursor = connection.cursor()
+        cursor.execute("UPDATE maintable "
+                       "SET name='"+name+"', date='"+date+"', weight="+weight+", dimensions_x="+dimensions_x+", "
+                        "dimensions_y="+dimensions_y+", dimensions_z="+dimensions_z+", composition='"+composition+"', qual_control="+qual_control+" WHERE name = '"+name+"';")
+        connection.commit()
+        connection.close()
+        cursor.close()
+        return "delete_sql"
+    except Error as e:
+        return "Error reading data from MySQL table"+str(e)
+
+
+data = "'pasha', '1997-10-04', 3.14, 0.1, 0.2, 0.4, 'oxygen2', 1"
 #print(delete_sql("polya_name"))
 #print(select_sql())
 #print(insert_into_sql(data))
--- tkinter-example1.py
+++ tkinter-example1.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-#from sevaSQL import *
+from sevaSQL import *
 from sevaQR import *
 import os
 
@@ -19,10 +19,27 @@ def print_QR_code():
     global name
     os.startfile(name.get()+".png", "print")
 
+def load_from_db():
+    global name_for_load, name, date, weight, dimensions_x, dimensions_y, dimensions_z, composition, qual_control
+    col = find_sql(name_for_load.get())
+    print(col[0][1])
+
+    name.set(col[0][1])
+    date.set(col[0][2])
+    weight.set(col[0][3])
+    dimensions_x.set(col[0][4])
+    dimensions_y.set(col[0][5])
+    dimensions_z.set(col[0][6])
+    composition.set(col[0][7])
+    qual_control.set(col[0][8])
+
+def load_to_db():
+    update_sql(name.get(), date.get(), weight.get(), dimensions_x.get(), dimensions_y.get(), dimensions_z.get(), composition.get(), qual_control.get())
 
 root = Tk()
-root.title("GUI на Python")
+root.title("App")
 
+name_for_load = StringVar()
 name = StringVar()
 date = StringVar()
 weight = StringVar()
@@ -32,48 +49,59 @@ dimensions_z = StringVar()
 composition = StringVar()
 qual_control = StringVar()
 
+name_for_load_label = Label(text="Название для внесения изменений:")
+name_for_load_label.grid(row=0, column=0, sticky="w")
+empty_label = Label(text=" ")
+empty_label.grid(row=1, column=0, sticky="w")
 name_label = Label(text="Название заготовки:")
-name_label.grid(row=0, column=0, sticky="w")
+name_label.grid(row=2, column=0, sticky="w")
 date_label = Label(text="Дата изготовления:")
-date_label.grid(row=1, column=0, sticky="w")
+date_label.grid(row=3, column=0, sticky="w")
 weight_label = Label(text="Вес заготовки:")
-weight_label.grid(row=2, column=0, sticky="w")
+weight_label.grid(row=2+2, column=0, sticky="w")
 dimensions_x_label = Label(text="Геометрические параметры заготовки (X):")
-dimensions_x_label.grid(row=3, column=0, sticky="w")
+dimensions_x_label.grid(row=3+2, column=0, sticky="w")
 dimensions_y_label = Label(text="Геометрические параметры заготовки (Y):")
-dimensions_y_label.grid(row=4, column=0, sticky="w")
+dimensions_y_label.grid(row=4+2, column=0, sticky="w")
 dimensions_z_label = Label(text="Геометрические параметры заготовки (Z):")
-dimensions_z_label.grid(row=5, column=0, sticky="w")
+dimensions_z_label.grid(row=5+2, column=0, sticky="w")
 composition_label = Label(text="Состав:")
-composition_label.grid(row=6, column=0, sticky="w")
+composition_label.grid(row=6+2, column=0, sticky="w")
 qual_control_label = Label(text="Отметка о прохождении контроля качества:")
-qual_control_label.grid(row=7, column=0, sticky="w")
+qual_control_label.grid(row=7+2, column=0, sticky="w")
 gen_qr_label = Label(text="Сгенерировать QR:")
-gen_qr_label.grid(row=8, column=0, sticky="w")
+gen_qr_label.grid(row=8+2, column=0, sticky="w")
 print_qr_label = Label(text="Напечатать QR:")
-print_qr_label.grid(row=9, column=0, sticky="w")
+print_qr_label.grid(row=9+2, column=0, sticky="w")
 
+name_for_load = Entry(textvariable=name_for_load)
+name_for_load.grid(row=0, column=1, padx=3, pady=3)
 name_entry = Entry(textvariable=name)
-name_entry.grid(row=0, column=1, padx=3, pady=3)
+name_entry.grid(row=0+2, column=1, padx=3, pady=3)
 date_entry = Entry(textvariable=date)
-date_entry.grid(row=1, column=1, padx=3, pady=3)
+date_entry.grid(row=1+2, column=1, padx=3, pady=3)
 weight_entry = Entry(textvariable=weight)
-weight_entry.grid(row=2, column=1, padx=3, pady=3)
+weight_entry.grid(row=2+2, column=1, padx=3, pady=3)
 dimensions_x_entry = Entry(textvariable=dimensions_x)
-dimensions_x_entry.grid(row=3, column=1, padx=3, pady=3)
+dimensions_x_entry.grid(row=3+2, column=1, padx=3, pady=3)
 dimensions_y_entry = Entry(textvariable=dimensions_y)
-dimensions_y_entry.grid(row=4, column=1, padx=3, pady=3)
+dimensions_y_entry.grid(row=4+2, column=1, padx=3, pady=3)
 dimensions_z_entry = Entry(textvariable=dimensions_z)
-dimensions_z_entry.grid(row=5, column=1, padx=3, pady=3)
+dimensions_z_entry.grid(row=5+2, column=1, padx=3, pady=3)
 composition_entry = Entry(textvariable=composition)
-composition_entry.grid(row=6, column=1, padx=3, pady=3)
+composition_entry.grid(row=6+2, column=1, padx=3, pady=3)
 qual_control_entry = Entry(textvariable=qual_control)
-qual_control_entry.grid(row=7, column=1, padx=3, pady=3)
+qual_control_entry.grid(row=7+2, column=1, padx=3, pady=3)
 
+
+gen_qr_button = Button(text="Загрузить из БД: ", command=load_from_db)
+gen_qr_button.grid(row=0, column=2, padx=3, pady=3, sticky="e")
+gen_qr_button = Button(text="Загрузить в БД: ", command=load_to_db)
+gen_qr_button.grid(row=0, column=3, padx=3, pady=3, sticky="e")
 gen_qr_button = Button(text="Сгенерировать QR code", command=generate_QR_code)
-gen_qr_button.grid(row=8, column=1, padx=3, pady=3, sticky="e")
+gen_qr_button.grid(row=8+2, column=1, padx=3, pady=3, sticky="e")
 print_qr_button = Button(text="Напечатать QR code", command=print_QR_code)
-print_qr_button.grid(row=9, column=1, padx=3, pady=3, sticky="e")
+print_qr_button.grid(row=9+2, column=1, padx=3, pady=3, sticky="e")
 
 
 #print(sevaSQL.select_sql())
